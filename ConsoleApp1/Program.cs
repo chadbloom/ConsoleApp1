fee1454878a8c2938dc0cc34eedf884070cf9ce4
blah <blah@LAPTOP-L0U8JIKI>
2020-01-16 22:09:06 -0500

    Commit the changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Here is a change");
+            Console.WriteLine("Here is another");
+        }
+    }
+}
