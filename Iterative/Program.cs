bacea03104e3b4f1055337e7ad6e93919e4a9928
blah <blah@LAPTOP-L0U8JIKI>
2020-02-18 10:07:54 -0500

    more

--- Iterative/Program.cs
+++ Iterative/Program.cs
@@ -9,6 +9,8 @@ namespace Iterative
             string n;
             Console.WriteLine("Please enter an integer from 2 to 10");
             n = Console.ReadLine();
+            int m;
+            m = Convert.ToInt32(n);
         }
     }
 }
