a601d531c3a26e6aa4b50e739a2fcb0468995b2f
blah <blah@LAPTOP-L0U8JIKI>
2020-02-24 22:41:39 -0500

    meth 1

---
+++ Methodizer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Methodizer
+{
+    class Program
+    {
+                // This is the main operation
+        static void Main(string[] args)
+        {
+            String a, b;
+            int x, y; //, c;
+            PrintName();
+            Console.WriteLine("Enter the first value:");
+            a = Console.ReadLine();
+            Console.WriteLine("Enter the second value:");
+            b = Console.ReadLine();
+            x = Convert.ToInt32(a);
+            y = Convert.ToInt32(b);
+
+            AddInputs(x, y);
+            print(c);
+
+            // this method prints my name
+            static void PrintName()
+            {
+                Console.WriteLine("Chad Bloom");
+            }
+
+            // this method performs the addition
+            static int AddInputs(int x, int y)
+            {
+                int c;
+                c = x + y;
+                return c;
+            }
+        }
+    }
+}
